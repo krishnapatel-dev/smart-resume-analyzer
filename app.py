6450e7d8899da4c50a43df148ab7fb20f49bce2a
Patel Krishna <patelkrishna01062003@gmail.com>
2026-03-08 21:20:28 +0530

    Added job description input feature

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ st.title("Smart Resume Analyzer")
 
 st.write("Upload your resume and compare it with a job description.")
 
-resume = st.file_uploader("Upload your Resume", type=["pdf","txt"])
+# Upload resume
+resume = st.file_uploader("Upload your Resume", type=["txt"])
+
+resume_text = ""
 
 if resume:
+    resume_text = resume.read().decode("utf-8")
     st.success("Resume uploaded successfully!")
+
+    st.subheader("Resume Content")
+    st.write(resume_text)
+
+
+# Job description input
+st.subheader("Paste Job Description")
+
+job_description = st.text_area("Enter the Job Description")
+
+if job_description:
+    st.subheader("Job Description")
+    st.write(job_description)
